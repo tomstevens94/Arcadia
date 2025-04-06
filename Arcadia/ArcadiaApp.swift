465f5f22f37a24dad81fefd38845b04a6aa012de
Thomas Stevens <thomas.stevens@axapppuk.onmicrosoft.com>
2025-04-06 10:29:50 +0100

    Initial Commit

---
+++ Arcadia/ArcadiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcadiaApp.swift
+//  Arcadia
+//
+//  Created by Tom Stevens on 06/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ArcadiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArcadiaTests/ArcadiaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArcadiaTests.swift
+//  ArcadiaTests
+//
+//  Created by Tom Stevens on 06/04/2025.
+//
+
+import Testing
+@testable import Arcadia
+
+struct ArcadiaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
